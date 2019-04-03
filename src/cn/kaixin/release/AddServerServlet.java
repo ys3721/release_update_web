e1aa562e9ddd6d4904910002f91dc6cbe78f0cd5
Yao Shuai <ys3721@hotmail.com>
2019-04-03 20:38:30 +0800

    1.添加一个输入框 用于生成配置文件。

--- src/cn/kaixin/release/AddServerServlet.java
+++ src/cn/kaixin/release/AddServerServlet.java
@@ -1,25 +1,26 @@
 package cn.kaixin.release;
 
 import cn.kaixin.release.constants.ReleaseServerConstants;
-import sun.net.httpserver.HttpServerImpl;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class AddServerServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String serverId = req.getParameter("serverId");
-        String domain = req.getParameter("domain");
-        String lanIp = req.getParameter("lanIp");
-        String wanIp = req.getParameter("wanIp");
-        String serverName = req.getParameter("prefix");
+        String serverId = req.getParameter("serverId").toLowerCase();
+        String domain = req.getParameter("domain").toLowerCase();
+        String lanIp = req.getParameter("lanIp").toLowerCase();
+        String wanIp = req.getParameter("wanIp").toLowerCase();
+        String serverName = req.getParameter("prefix").toLowerCase();
 
-        String content = String.format("%s 1 %s %s %s %s true", serverId, domain, lanIp, wanIp, serverName);
+        String content = String.format("%s 1 %s %s %s %s true\n", serverId, domain, lanIp, wanIp, serverName);
 
         File file = new File(ReleaseServerConstants.SERVERS_CONFIG_DIR+serverName+".config");
         PrintWriter writer = new PrintWriter(new FileOutputStream(file));
---
+++ src/cn/kaixin/release/RunDeployServlet.java
@@ -0,0 +1,14 @@
+package cn.kaixin.release;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RunDeployServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Runtime.getRuntime().exec("");
+    }
+}
