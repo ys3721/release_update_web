3447e0970af5aa9c832f5c3700068a0d76928b36
Shuai Yao <ys3721@hotmail.com>
2019-04-10 00:06:42 +0800

    写了一些过滤文件的正则表达式。

--- src/cn/kaixin/release/AddServerServlet.java
+++ src/cn/kaixin/release/AddServerServlet.java
@@ -5,10 +5,8 @@ import cn.kaixin.release.constants.ReleaseServerConstants;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -27,11 +25,47 @@ public class AddServerServlet extends HttpServlet {
 
         String content = String.format("%s 1 %s %s %s %s true\n", serverId, domain, lanIp, wanIp, serverName);
 
+        String[] configFileStrs = new File(ReleaseServerConstants.SERVERS_CONFIG_DIR).list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                Pattern pattern = Pattern.compile("\\w*.config");
+                return pattern.matcher(name).matches();
+            }
+        });
+
+        for (String configFileName : configFileStrs) {
+            File file = new File(ReleaseServerConstants.SERVERS_CONFIG_DIR+configFileName);
+            if(file.isFile()) {
+                FileReader reader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                String configLine = bufferedReader.readLine();
+                String[] configSplit = configLine.split("\\s+");
+                if (configSplit.length < 6) {
+                    continue;
+                }
+                if(configSplit[3].trim().equals(lanIp) || configSplit[4].trim().equals(wanIp)) {
+                    resp.setHeader("Content-Type","text/html;charset=UTF-8");
+                    resp.getWriter().println("<html><body>");
+                    resp.getWriter().println(String.format("<p>ERROR!:IP已经存在不能覆盖！！%s %s %s", configFileName, lanIp,wanIp));
+                    resp.getWriter().println("</body></html>");
+                    return;
+                }
+                if (configSplit[0].equals(serverId)) {
+                    resp.setHeader("Content-Type","text/html;charset=UTF-8");
+                    resp.getWriter().println("<html><body>");
+                    resp.getWriter().println(String.format("<p>ERROR!:SERVER_ID已经存在不能覆盖！！%s %s", configFileName, serverId));
+                    resp.getWriter().println("</body></html>");
+                    return;
+                }
+
+            }
+        }
+
         File file = new File(ReleaseServerConstants.SERVERS_CONFIG_DIR + serverName+".config");
         if (file.exists()) {
             resp.setHeader("Content-Type","text/html;charset=UTF-8");
             resp.getWriter().println("<html><body>");
-            resp.getWriter().println("<p>ERROR!:已经存在不能覆盖！！");
+            resp.getWriter().println("<p>ERROR!:同名配置文件已经存在不能覆盖！！");
             resp.getWriter().println("</body></html>");
         }
         PrintWriter writer = new PrintWriter(new FileOutputStream(file));
