ba577a927c17af280a7ce75659e85c8fa1d53122
Yao Shuai <ys3721@hotmail.com>
2019-08-11 19:50:25 +0800

    银杏爱红警 进球瓜海爱红英

--- src/cn/kaixin/release/constants/ReleaseServerConstants.java
+++ src/cn/kaixin/release/constants/ReleaseServerConstants.java
@@ -2,7 +2,7 @@ package cn.kaixin.release.constants;
 
 public class ReleaseServerConstants {
 
-    public static final String SERVERS_CONFIG_DIR = "/Users/yaoshuai/data2/servers";
+    public static final String SERVERS_CONFIG_DIR = "/data2/servers";
 
     public static final String WORK_BASE_DIR = "/data3/init_server/";
 
